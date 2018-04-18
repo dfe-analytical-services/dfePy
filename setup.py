420ec9443fab8c8884b2cfd274efccaef19a9afb
CHANG <Eugene.CHANG@EDUCATION.GOV.UK>
2018-04-18 09:45:34 +0100

    Altered package structure and added new functions

--- setup.py
+++ setup.py
@@ -10,18 +10,21 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='python-functions',
-    url='https://github.com/DFEAGILEDEVOPS/python-functions',
+    # *strongly* suggested for sharing
+    version='0.1',
+    url='https://github.com/dfe-analytical-services/python-functions',
     author='Eugene Chang',
     author_email='eugene.chang@education.gov.uk',
     # Needed to actually package something
     packages=['dfe_python'],
     # Needed for dependencies
-    install_requires=['pandas','pymssql'],
-    # *strongly* suggested for sharing
-    version='0.1',
+    install_requires=['pandas','pymssql', 'requests'],
+    scripts = ["run_dfe.py"],
     # The license can be anything you like
     license='MIT',
     description='Repository of re-useable Python functions in DfE',
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
+    #classifiers contains information for PyPI, which we ignore for now.
+    classifiers= [],
 )
